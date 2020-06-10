8e38ac939fb299b683d322ce97dee19369a10df7
rospiero <rospiero@gmail.com>
2020-06-10 17:15:09 +0200

    (a pdf file combinator)

---
+++ combinatore.py
@@ -0,0 +1,47 @@
+import PyPDF2
+import os
+from PyPDF2 import PdfFileMerger, PdfFileReader
+
+#you should insert here the path to the directory where the pdf are
+os.chdir("C:\\Python")
+
+
+
+#depending on how many file you have, if you want to open them and check you can put the name of the file and rb
+pdf1File=open("A.pdf", "rb")
+pdf2File=open("B.pdf", "rb")
+
+#the file will be then readed and saved in variables
+reader1= PyPDF2.PdfFileReader(pdf1File)
+reader2= PyPDF2.PdfFileReader(pdf2File)
+
+
+writer= PyPDF2.PdfFileWriter()
+
+#extracting the text
+
+for pageNum in range(reader1.numPages):
+    print(reader1.getPage(pageNum).extractText())
+#
+#
+for pageNum in range(reader2.numPages):
+    print(reader2.getPage(pageNum).extractText())
+#
+#
+#
+#
+for page in range(reader1.numPages):
+    page=reader1.getPage(pageNum)
+    writer.addPage(page)
+#
+for page in range(reader2.numPages):
+    page=reader2.getPage(pageNum)
+    writer.addPage(page)
+
+
+
+#the final file!
+outputfile= open("mixitall.pdf", "wb")
+writer.write(outputfile)
+
+outputfile.close
